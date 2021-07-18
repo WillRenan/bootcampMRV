932f350e76b751b043f6ced7423daef5d799de48
WillRenan <willdesign.will@gmail.com>
2021-07-18 16:29:54 -0300

    introdução a javascript  MRV bootcamp

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+ function btnClicado(){
+    let name = prompt('qual seu nome?');
+    alert(`Olá ${name} ! Para mais informações confira os links ao fim dessa página,obrigado!`);
+    window.location.href='#info';
+ }
