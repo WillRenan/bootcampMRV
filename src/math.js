7b11c726d1e910d52387c900bdb4154953e86295
WillRenan <willdesign.will@gmail.com>
2021-07-23 23:10:12 -0300

    Instalando e aplicando ferramentas de teste

--- src/math.js
+++ src/math.js
@@ -1,7 +1,21 @@
 class Math{
-  sum = function sum(){
-
+ /*  sum(a,b,){
+    return a+b;
+  } */
+  sum(a,b, callback){
+    setTimeout(()=>{
+      callback(a+b);
+    },0);
+    
+  }
+  multiply(a,b){
+    return (a*b);
+  }
+  printSum(req,res,a,b){
+    res.load('index',a+b);
   }
 }
+
+
 
 module.exports = Math;
--- test/math.spec.js
+++ test/math.spec.js
@@ -1,14 +1,61 @@
 const assert = require('assert');
 const Math = require('../src/math.js');
+const expect = require('chai').expect;//import lib chai
+const sinon = require('sinon');
+//https://www.chaijs.com
 
+//teste assíncrono com o callback de uma função 
+var valor  = 0 ;
 describe('Math class', function(){
-  it('Soma dois números',function(){
+
+  //Hooks- faz com que valores sejam resetado cl
+  beforeEach(function(){
+    valor = 0;
+  })
+  it('Soma dois números',function(done){ //Descrevendo o teste
 
     const math = new Math();
-    try{
-    assert.equal(math.sum(5,5), 10);
-    } catch(error){
-      console.log(error)
+    this.timeout(3000);
+
+    valor = 5;
+
+    math.sum(5,valor,(value)=>{
+      //assert.equal(value,10);
+      expect(value).to.equal(10);
+      done();
+    });    
+  });
+  //it.only executa somente este teste
+  //it.skip  pula este teste
+  it(`Multiplicar
+      dois números`, function(){
+    const math = new Math();
+
+    //assert.equal(math.multiply(valor,5), 25);
+    expect(math.multiply(valor,5)).to.equal(0);
+  })
+
+  it('Conferindo objetos', function(){
+    const obj ={
+      nome:'Willian Renan'
     }
+    const obj2 ={
+      nome:'Willian Renan'
+    }
+    expect(obj).to.have.property('nome');//verificando a existência de uma propriedade específica 
+    expect(obj).to.have.property('nome').equal('Willian Renan'); //verificando a existente do valor do objeto
+    expect(obj).to.deep.equal(obj2);//comparando as propriedades de dois objetos
+  })
+
+  it.only('Verificando chamada de função', function(){
+    const req = {};
+    const res = {
+      load: sinon.spy()
+    };
+    const math = new Math();
+    math.printSum(req,res,5,5);
+    expect(res.load.calledOnce).to.be.true; //fica observando se uma função foi executada ou não, neste caso é o método printSum do Math
+    
+ 
   });
 });
